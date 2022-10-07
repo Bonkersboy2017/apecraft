c9d58308254d4783b8a81cd582d30aa31ca43fc8
Treetrain1 <treetrain1@gmail.com>
2022-10-06 21:30:41 -0500

    better entity AI?

--- src/main/java/com/apedev/apecraft/registry/entities/BonoboEntity.java
+++ src/main/java/com/apedev/apecraft/registry/entities/BonoboEntity.java
@@ -2,9 +2,9 @@ package com.apedev.apecraft.registry.entities;
 
 import com.apedev.apecraft.items.RegisterItems;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.TemptGoal;
-import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.world.World;
 
@@ -15,7 +15,11 @@ public class BonoboEntity extends PathAwareEntity {
 
 
     protected void initGoals() {
-        this.goalSelector.add(7, new WanderAroundFarGoal(this, 0.4D));
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new EscapeDangerGoal(this, 1.25));
         this.goalSelector.add(4, new TemptGoal(this, 0.7D, Ingredient.ofItems(RegisterItems.TWINE), false));
+        this.goalSelector.add(6, new WanderAroundFarGoal(this, 0.4D));
+        this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.add(8, new LookAroundGoal(this));
     }
 }
